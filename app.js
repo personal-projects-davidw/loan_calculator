48321f324ecd5a0eee14b853c7fdf938f4e2ba18
David Weatherspoon <david_weatherspoon@hotmail.com>
2020-06-05 22:11:09 +0100

    Completed with calculation functions now added & results display.

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
+function calcResults() {
+    const principle = parseFloat(amountSlider.value);
+    const calculatedInterest = parseFloat(aprSlider.value) / 100 / 12;
+    const calculatedPayments = parseFloat(termSlider.value);
+
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principle * x * calculatedInterest)/(x-1);
+
+    monthlyRepaymentOutput.innerHTML = `£${monthly.toFixed(2)}`;
+    totalPayableOutput.innerHTML = `£${(monthly * termSlider.value).toFixed(2)}`;
+}
+
 let amountSlider = document.querySelector("#amount");
 let termSlider = document.querySelector("#term");
 let aprSlider = document.querySelector("#apr");
 
-let amount = document.querySelector(".output.amount");
-let term = document.querySelector(".output.term");
-let apr = document.querySelector(".output.apr");
+let amountOutput = document.querySelector(".output.amount");
+let termOutput = document.querySelector(".output.term");
+let aprOutput = document.querySelector(".output.apr");
 
-amountSlider.onchange = () => {
-    amount.innerHTML = `£${amountSlider.value}`;
+let monthlyRepaymentOutput = document.querySelector("#monthly");
+let totalPayableOutput = document.querySelector("#total");
+
+amountSlider.oninput = () => {
+    amountOutput.innerHTML = `£${amountSlider.value}`;
+    calcResults();
 }
 
-termSlider.onchange = () => {
-    term.innerHTML = `${termSlider.value}`;
+termSlider.oninput = () => {
+    termOutput.innerHTML = `${termSlider.value}`;
+    calcResults();
 }
 
-aprSlider.onchange = () => {
-    apr.innerHTML = `${aprSlider.value}%`;
+aprSlider.oninput = () => {
+    aprOutput.innerHTML = `${aprSlider.value}%`;
+    calcResults();
 }
+
+calcResults();
